268cc051bfff51277d88d5101d8b9b6692a8216c
Victor Humberto <107077339+Victorhumbert@users.noreply.github.com>
2024-04-25 15:28:25 +0000

    modificado frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <h1>Sabrina Chaussard Osmo, eu te amo princesa</h1>)
+        <h1>Sabrina Chaussard Osmo, eu te amo princesa, prefeita❤️❤️</h1>)
 }
 
 export default Home;
