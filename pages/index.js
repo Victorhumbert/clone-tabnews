0c28a4375b56a55a440d5de36c7d156571067a71
Victor Humberto <107077339+Victorhumbert@users.noreply.github.com>
2024-05-23 16:19:18 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return (
-        <h1>Sabrina Chaussard Osmo, eu te amo princesa, prefeita❤️❤️</h1>)
+  return <h1>Sabrina Chaussard Osmo, eu te amo princesa, prefeita❤️❤️</h1>;
 }
 
 export default Home;
