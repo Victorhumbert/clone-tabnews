35fe4bb71f3c74d4056a7d63328889f2e08f375c
Victor Humberto <107077339+Victorhumbert@users.noreply.github.com>
2024-12-23 10:12:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Sabrina Chaussard Osmo, eu te amo princesa, prefeita❤️❤️</h1>;
+  return (
+    <div>
+      <h1>Sabrina Chaussard Osmo, eu te amo princesa linda❤️❤️</h1>
+      <h2>Agora, se você me ama, da uma risadinha</h2>
+    </div>
+  );
 }
 
 export default Home;
