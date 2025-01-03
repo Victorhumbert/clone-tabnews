d5c7cc96babee8f77e3687372111e1e394e5a143
Victor Humberto <107077339+Victorhumbert@users.noreply.github.com>
2025-01-03 03:59:33 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as soma;");
-  console.log(result.rows[0]);
-  response
-    .status(200)
-    .send({ message: "Alunos do curso.dev são pessoas acima da média" });
+  const updateAt = new Date().toISOString();
+  const maxConections = await database.query("SHOW max_connections;");
+  const postgresVersion = await database.query("SHOW server_version;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).send({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        max_connections: +maxConections.rows[0].max_connections,
+        opened_connections: usedConections.rows[0].count,
+      },
+    },
+  });
 }
 export default status;
