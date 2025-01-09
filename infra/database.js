3d79e779eadc9d7e7f9a4ea4cad22e1ef622c225
Victor Humberto <107077339+Victorhumbert@users.noreply.github.com>
2025-01-09 03:12:30 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,13 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  console.log({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   try {
@@ -21,7 +15,7 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error("Erro ao executar a query", error);
+    console.error(error);
     throw error;
   } finally {
     await client.end();
